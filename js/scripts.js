18fce3ae9190784b65ae758af894a8a5f8c0491f
Juan Moreno <morenostudent1@gmail.com>
2021-06-18 03:29:27 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,7 @@
 *
 */
 
-let msg = “ Hi 👋! Welcome to my site!”;
-let styles= [ 
-    ‘font-size: 12px’, 
-    ‘font-family: monospace’, 
-    ‘background: white’, 
-    ‘display: inline-block’, 
-    ‘color: black’, 
-    ‘padding: 8px 19px’, 
-    ‘border: 1px dashed;’ 
-].join(‘;’) 
-console.log(msg, styles)
+console.log(“ Hi 👋! Welcome to my site!”)
 
 window.addEventListener('DOMContentLoaded', event => {
 
