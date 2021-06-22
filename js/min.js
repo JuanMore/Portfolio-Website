ffa7a6b3f9acb0858d67e6b26a2fd1c007f73e67
Juan Moreno <morenostudent1@gmail.com>
2021-06-21 20:47:48 -0700

    Update min.js

--- js/min.js
+++ js/min.js
@@ -164,7 +164,7 @@
           , h = n("#submitSuccessMessage", o)
           , f = n("#submitErrorMessage", o)
           , b = o.dataset.sbFormApiToken;
-        "33fd980a-527e-4823-8966-10d414f155ba" === b && (a = !1,
+        "API_TOKEN" === b && (a = !1,
         console.log(l, u));
         const m = t("input, textarea, select", o);
         m.forEach((e=>{
@@ -258,7 +258,7 @@
             var n, s, i, o
         }
         ));
-        v = ()=>{
+        let v = ()=>{
             m.reduce(((e,t)=>"yes" === t.dataset.sbCanSubmit ? e && !0 : e && !1), !0) ? c.classList.remove("disabled") : c.classList.add("disabled")
         }
     }
